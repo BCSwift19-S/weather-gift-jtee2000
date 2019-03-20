e9c9f8864642f8bb83fc108c1da00a0682862b16
Jason Tee <Jason@macbook-pro.local>
2019-03-20 03:25:25 -0400

    Finished 7.11

---
+++ WeatherGift/WeatherLocatio.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocatio.swift
+//  WeatherGift
+//
+//  Created by Jason Tee on 3/20/19.
+//  Copyright © 2019 Jason Tee. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name = ""
+    var coordinates = ""
+}

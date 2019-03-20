0ca2d126f6895415dd69d2caf050cecbd1f719a6
Jason Tee <Jason@macbook-pro.local>
2019-03-20 04:34:24 -0400

    Completed 7.15

---
+++ WeatherGift/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  WeatherGift
+//
+//  Created by Jason Tee on 3/20/19.
+//  Copyright © 2019 Jason Tee. All rights reserved.
+//
+
+import Foundation
+
+let urlBase = "https://api.darksky.net/forecast/"
+let urlAPIKey = "51bfafc97d4e506dfe37bc60e7d381eb/"
+

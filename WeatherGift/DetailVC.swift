ba4ffd89f7e210664d54b51cecf2e9a59f5aeb36
Jason Tee <Jason@macbook-pro.local>
2019-03-24 22:54:16 -0400

    Completed 7.24

--- WeatherGift/DetailVC.swift
+++ WeatherGift/DetailVC.swift
@@ -64,6 +64,7 @@ class DetailVC: UIViewController {
             summaryLabel.text = location.currentDescription
             currentImage.image = UIImage(named: location.currentIcon)
             tableView.reloadData()
+            collectionView.reloadData()
     
     }
     
@@ -145,11 +146,14 @@ extension DetailVC: UITableViewDelegate, UITableViewDataSource {
 
 extension DetailVC: UICollectionViewDataSource, UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 24
+        return locationsArray[currentPage].hourlyForecastArray.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let hourlyCell = collectionView.dequeueReusableCell(withReuseIdentifier: "HourlyCell", for: indexPath)
+        let hourlyCell = collectionView.dequeueReusableCell(withReuseIdentifier: "HourlyCell", for: indexPath) as! HourlyWeatherCell
+        let hourlyForecast = locationsArray[currentPage].hourlyForecastArray[indexPath.row]
+        let timeZone = locationsArray[currentPage].timeZone
+        hourlyCell.update(with: hourlyForecast, timeZone: timeZone)
         return hourlyCell
     }
     
